138778df0e15268b4b124f635cfca4e576d3d430
CarelKempVonMolendorff <carel.von.molendorff@epiuse.com>
2024-10-22 10:41:06 +0200

    Barcode Library

--- pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.js
+++ pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.js
@@ -6,3 +6,69 @@
 
 // 	},
 // });
+
+import JsBarcode from "jsbarcode";
+
+frappe.ui.form.ControlBarcode = class ControlBarcode extends frappe.ui.form.ControlData{
+    parse(value){
+        if(value){
+            if (value.startsWith("<svg>")){
+                return value;
+            }
+            return this.get_barcode_html(value);
+        }
+    }
+
+    set_formatted_input(value){
+        let svg = value;
+        let barcode_value = "";
+
+        this.set_empty_description();
+        if(value && value.startsWith("<svg>")){
+            barcode_value = $(svg).attr("data-barcode-value");
+        }
+
+        if (!barcode_value && this.doc){
+            svg = this.get_barcode_html(value);
+            this.doc[this.df.filenamne] = svg;
+        }
+
+        this.$input.val(barcode_value || value);
+        this.barcode_area.html(svg || this.default_svg);
+    }
+
+    get_barcode_html(value){
+        if(value){
+            const svg = this.barcode_area.find("svg")[0];
+            try{
+                JsBarcode(svg, value, this.get_options(value));
+                $(svg).attr("data-barcode-value", value);
+                $(svg).attr("width", "100%");
+                return this.barcode_area.html();
+            } catch(e){
+                this.set_description(`Invalid Barcode: ${String(e)}`);
+            }
+        }
+    }
+
+    get_options(value){
+        let options = {};
+        options.fontsize = "16";
+        options.width = "3";
+        options.height = "50";
+
+        if(frappe.utils.is_json(this.df.options)){
+            options - JSON.parse(this.df.options);
+            if(options.format === "EAN") {
+                options.format = value.length == 8? "EAN8" : "EAN13";
+            }
+
+            if(options.valueField){
+                this.frm && this.frm.set_value(options.valueField, value);
+            }
+        }
+
+        return options;
+    }
+
+}
