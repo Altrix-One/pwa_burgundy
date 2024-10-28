4e09bbf729c4139a36e1fe47972c0d0eccc92fa0
CarelKempvonMolendorff <carel.von.molendorff@epiuse.com>
2024-10-28 06:40:04 +0200

    initial Commit

--- pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.js
+++ pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, Aerele Technologies Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("PWA Form", {
-// 	refresh(frm) {
-
-// 	},
-// });
+codefrappe.ui.form.on('PWA Form', {
+    refresh: function(frm) {
+        // Initialize the BarcodeScannerconst 
+        scanner = new erpnext.utils.BarcodeScanner({
+            on_scan: function(barcode) {
+                // Automatically populate the barcode field with the scanned value                
+                frm.set_value('scan_barcode', barcode);
+                frappe.show_alert({message: `Barcode scanned: ${barcode}`, indicator: 'green'});
+            }        });
+        // Trigger the barcode scanner when user selects the barcode field        
+        frm.fields_dict.scan_barcode.$input.on('focus', function() {
+            scanner.start();
+        });        
+        // Stop scanning when field is out of focus
+        frm.fields_dict.scan_barcode.$input.on('blur', function() {
+            scanner.stop();
+        });
+    }
+});
