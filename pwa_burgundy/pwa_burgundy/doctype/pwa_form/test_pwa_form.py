fcf3435177b82668f635f78ac504b2c022fccd2e
Christiaan Swart <christiaan.swart@epiuse.com>
2024-10-28 08:26:44 +0200

    Barcode Test

--- pwa_burgundy/pwa_burgundy/doctype/pwa_form/test_pwa_form.py
+++ pwa_burgundy/pwa_burgundy/doctype/pwa_form/test_pwa_form.py
@@ -3,7 +3,17 @@
 
 # import frappe
 from frappe.tests.utils import FrappeTestCase
+import unittest
 
+class TestPWAForm(unittest.TestCase):
+    def test_barcode_scan(self):
+        # Simulate a barcode scan and test its effect
+        doc = frappe.get_doc({
+            "doctype": "PWA Form",
+            "scan_barcode": "123456789012"
+        })
+        doc.insert()
+        self.assertEqual(doc.scan_barcode, "123456789012")
 
 class TestPWAForm(FrappeTestCase):
 	pass
