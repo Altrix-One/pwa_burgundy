a426014a230062efdacf268b5e9aa39849dd8752
CarelKempvonMolendorff <carel.von.molendorff@epiuse.com>
2024-10-30 10:10:52 +0200

    additional Python code

--- pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.py
+++ pwa_burgundy/pwa_burgundy/doctype/pwa_form/pwa_form.py
@@ -1,10 +1,8 @@
 # Copyright (c) 2024, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
-    # You can add custom server-side validations hereif self.scan_barcode:
-    # Perform any validation or processing on the scanned barcode
 # import frappe
 import frappe
 class PWAForm(Document):
     def validate(self):
-	if not frappe.db.exists("Item", {"barcode": self.scan_barcode}):
-  		frappe.throw(f"Barcode {self.scan_barcode} does not exist in the system.")
+        if not frappe.db.exists("Item", {"barcode": self.scan_barcode}):
+            frappe.throw(f"Barcode {self.scan_barcode} does not exist in the system.")
